7bf595694b7bdbfc00f78a203f272a755fd998a7
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-21 14:50:32 +0200

    21/04/2017-14:50:32,42

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $router->setNotFound(function($url) use ($view){
     $view->display('404.php');
 });
 
-$controller = new Controller(BASEPATH.'/controllers/', [
+$controller = new Controller('/controllers/', [
     'view' => $view,
     'session' => $session,
     'router' => $router,
