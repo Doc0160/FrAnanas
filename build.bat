c5c7b68147778f772837e782aa32a43d3cdad68b
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-21 16:54:08 +0200

    21/04/2017-16:54:07,20

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo off
+set msg=%*
+if "%msg%" equ "" (
+   set msg=%date%-%time%
+)
 echo.
 echo --- git status ---
 git status
@@ -9,8 +13,8 @@ echo.
 echo --- git add . ---
 git add .
 echo.
-echo --- git commit -m "%date%-%time%" ---
-git commit -m "%date%-%time%"
+echo --- git commit -m "%msg%" ---
+git commit -m "%msg%"
 echo.
 echo --- git push ---
 git push
