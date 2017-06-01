4764f4beff41812173319745177f9bd7c20d188d
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-06-01 23:07:17 +0200

    01/06/2017-23:07:15,69

--- core/autoload/autoload.php
+++ core/autoload/autoload.php
@@ -1,16 +1,35 @@
 <?php
 /**
+ * Autoload System File
+ */
+
+/**
  * Class Autoloader
  */
 class Autoload{
 
+    /**
+     * @ignore
+     */
     private $path;
+    /**
+     * @ignore
+     */
     private $autoloadable = [];
     
+    /**
+     * Constructor
+     * @param string $path
+     */
     public function __construct(string $path = '') {
         $this->path = $path;
     }
 
+    /**
+     * Register new thing to be loaded
+     * @param string $name
+     * @param $loader
+     */
     public function register(string $name, $loader = false){
         if(is_callable($loader) || $loader == false){
             $this->autoloadable[$name] = $loader;
@@ -18,8 +37,12 @@ class Autoload{
         }
         throw new Exception('Loader must be callable '.$name);
     }
-
-    public function load(string $name){
+    
+    /**
+     * To be called be the autoload thingy
+     * @param string $name
+     */
+    public function load(string $name) : bool {
         $name = strtolower($name);
         $filepath = $this->path.'core/'.$name.'/'.$name.'.php';
         
